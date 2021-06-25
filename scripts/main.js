2726e69475ef97c18bc6feb80c4542bc9c7ebb59
Nikita Chukanov <nikita.chukanov@gmail.com>
2021-06-25 19:46:52 +0500

    fix: bug fixes after first code review

--- scripts/main.js
+++ scripts/main.js
@@ -4,38 +4,43 @@ let editPopup = container.querySelector('.popup_type_edit');
 let closeButton = container.querySelector('.popup__close-button');
 let popup = container.querySelector('.popup');
 // Находим форму в DOM
-let formElement = container.querySelector('.popup__form_type_edit');
+let formEdit = container.querySelector('.popup__form_type_edit');
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__form-input-name');
-let jobInput = formElement.querySelector('.popup__form-input-occupation');
+let nameInput = document.getElementById('#inputName');
+let jobInput = document.getElementById('#inputOccupation');
+// Выберите элементы, куда должны быть вставлены значения полей.
+let bio = container.querySelector('.profile__info-text')
+let editName = bio.querySelector('.profile__name')
+let editJob = bio.querySelector('.profile__occupation')
 
-editButton.addEventListener('click', function showEdit() {         //Отображение попапа Edit
+//Отображение попапа Edit
+function showEdit() {
   editPopup.classList.add('popup_status_opened');
-});
+}
 
-closeButton.addEventListener('click', function closePopup() {              //Закрытие попапов
+editButton.addEventListener('click', showEdit);
+//Закрытие попапов
+function closePopup() {
   popup.classList.remove('popup_status_opened');
-});
+}
+
+closeButton.addEventListener('click', closePopup);
 
 
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler (evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
-
-  // Получите значение полей jobInput и nameInput из свойства value
+function formSubmitHandler(evt) {
+  // Эта строчка отменяет стандартную отправку формы.
+  evt.preventDefault();
+  // Получите значение полей jobInput и nameInput из свойства value. При выносе за пределы функции обработчик не считывает данные. Исправить*
   let inputs = [nameInput.value, jobInput.value];
-  // Выберите элементы, куда должны быть вставлены значения полей
-  let editName = container.querySelector('.profile__name')
-  let editJob = container.querySelector('.profile__occupation')
-  // Вставьте новые значения с помощью textContent
+  // Вставьте новые значения с помощью textContent.
   editName.textContent = inputs[0];
   editJob.textContent = inputs[1];
+  closePopup();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
+formEdit.addEventListener('submit', formSubmitHandler);
