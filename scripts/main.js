586236e90ede7e7bda61cc73746f49e57fb0d352
Nikita Chukanov <nikita.chukanov@gmail.com>
2021-08-22 21:43:24 +0500

    feat: добавить возможность добавлять карточки

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,31 @@
 const container = document.querySelector(".page");
-
 const pictureTemplate = container.querySelector("#pictureCard").content; //Card template
 const picturesContainer = container.querySelector(".pictures__grid");
-
+const picturePopup = container.querySelector('.popup_type_picture');
 // Находим форму в DOM
 const formEdit = container.querySelector(".popup__form_type_edit");
 const formAdd = container.querySelector('.popup__form_type_add');
-// Находим поля формы в DOM
+// Находим поля формы edit в DOM
 const nameInput = document.getElementById("#inputName");
 const jobInput = document.getElementById("#inputOccupation");
+// Находим поля формы add в DOM
+const titleInput = document.getElementById("#inputTitle");
+const linkInput = document.getElementById("#inputLink");
 
-// Выберите элементы, куда должны быть вставлены значения полей.
+// Выберите элементы, куда должны быть вставлены значения полей. edit
 const bio = container.querySelector(".profile__info-text");
 const editName = bio.querySelector(".profile__name");
 const editJob = bio.querySelector(".profile__occupation");
+// Выберите элементы, куда должны быть вставлены значения полей. edit
 
 //закрытие popup
 function closePopup(popupElement) {
   popupElement.classList.remove("popup_status_opened");
 }
+
+picturePopup.querySelector('.popup__close-button').addEventListener('click', function () {
+  closePopup(picturePopup)
+});
 
 //открытие popup
 function showPopup(popupElement) {
@@ -51,17 +58,19 @@ addButton.addEventListener("click", function () {
   showPopup(addPopup);
 });
 
-// Обработчик «отправки» формы, хотя пока
+// Обработчик «отправки» формы edit, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler(evt) {
   // Эта строчка отменяет стандартную отправку формы.
   evt.preventDefault();
-  // Получите значение полей jobInput и nameInput из свойства value. При выносе за пределы функции обработчик не считывает данные. Исправить*
+  // Получите значение полей jobInput и nameInput из свойства value.
   const inputs = [nameInput.value, jobInput.value];
 
   if (inputs[0] !== "" && inputs[1] !== "") {
     editName.textContent = inputs[0]; // Вставьте новые значения с помощью textContent.
     editJob.textContent = inputs[1];
+    nameInput.value = '';
+    jobInput.value = '';
     closePopup(editPopup);
   }
 }
@@ -97,23 +106,45 @@ const initialCards = [
   },
 ];
 
+// добавление карточки
 function addCard(linkValue, titleValue) {
-  const pictureElement = pictureTemplate
-    .querySelector(".pictures__item")
-    .cloneNode(true);
-
+  const pictureElement = pictureTemplate.querySelector(".pictures__item").cloneNode(true);
   pictureElement.querySelector(".pictures__image").src = linkValue;
   pictureElement.querySelector(".pictures__title").textContent = titleValue;
-  pictureElement
-    .querySelector(".pictures__like-button")
-    .addEventListener("click", function (evt) {
-      pictureElement
-        .querySelector(".pictures__like-button")
-        .classList.toggle("pictures__like-button_status_active");
-    });
-  picturesContainer.append(pictureElement);
+  pictureElement.querySelector(".pictures__like-button").addEventListener("click", function () {
+    pictureElement.querySelector(".pictures__like-button").classList.toggle("pictures__like-button_status_active");
+  });
+  pictureElement.querySelector('.pictures__delete-button').addEventListener('click', function (evt) { //удаление карточки
+    removeCard(evt.target);
+  })
+  pictureElement.querySelector(".pictures__image").addEventListener('click', function () { //открытие попапа по клику на картинку
+    showPopup(picturePopup);
+    picturePopup.querySelector('.popup__picture').src = linkValue;
+    picturePopup.querySelector(".popup__picture-caption").textContent = titleValue;
+  })
+  picturesContainer.prepend(pictureElement);
 }
 
 initialCards.forEach(function (Element) {
   addCard(Element.link, Element.name);
 });
+
+// обработчик «отправки» формы add
+function addCardSubmit(evt) {
+  evt.preventDefault();
+  const inputs = [titleInput.value, linkInput.value];
+  if (inputs[0] !== "" && inputs[1] !== "") {
+    addCard(inputs[1], inputs[0]);
+    titleInput.value = '';
+    linkInput.value = '';
+    closePopup(addPopup);
+  }
+}
+formAdd.addEventListener("submit", addCardSubmit);
+
+//удаление карточек
+function removeCard(place) {
+  const pictureElement = place.parentElement;
+  pictureElement.remove();
+}
+
